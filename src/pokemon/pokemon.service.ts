30360ab1bc8d931195f141246b78446d063c1e21
dangelover <kevincastrocampos@gmail.com>
2025-01-09 15:59:19 -0500

    Se cambio el nombre

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,7 +9,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginationDto } from 'src/common/dto/Pagination.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
